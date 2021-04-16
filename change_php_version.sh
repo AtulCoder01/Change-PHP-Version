cc6a6c9854df1a7a7014ba0612538c4cc9038099
Atul Kumar <patelatul50@gmail.com>
2021-04-16 11:30:06 +0530

    Update change_php_version.sh
    changes

--- change_php_version.sh
+++ change_php_version.sh
@@ -3,47 +3,56 @@
 echo "Change PHP Version into CLI..."
 echo "sudo update-alternatives --config php"
 sudo update-alternatives --config php
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Disable the PHP$1 module..."
-echo -n "sudo a2dismod php$1"
+echo "sudo a2dismod php$1"
 sudo a2dismod php$1
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Restarting Apache2..."
-echo -n "sudo systemctl restart apache2"
+echo "sudo systemctl restart apache2"
 sudo systemctl restart apache2
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Disable the PHP$1 configration..."
-echo -n "sudo a2disconf php$1-fpm"
+echo "sudo a2disconf php$1-fpm"
 sudo a2disconf php$1-fpm
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Restarting Apache2..."
-echo -n "sudo systemctl restart apache2"
+echo "sudo systemctl restart apache2"
 sudo systemctl restart apache2
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Enable the PHP$2 module..."
-echo -n "sudo a2enmod php$2"
+echo "sudo a2enmod php$2"
 sudo a2enmod php$2
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Restarting Apache2..."
-echo -n "sudo systemctl restart apache2"
+echo "sudo systemctl restart apache2"
 sudo systemctl restart apache2
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Enable the PHP$2 configration..."
-echo -n "sudo a2enconf php$2-fpm"
+echo "sudo a2enconf php$2-fpm"
 sudo a2enconf php$2-fpm
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Restarting Apache2..."
-echo -n "sudo systemctl restart apache2"
+echo "sudo systemctl restart apache2"
 sudo systemctl restart apache2
-echo "  OK!"
+echo "Done!"
+echo ""
 
 echo "Check PHP Version..."
 echo "php -v"
